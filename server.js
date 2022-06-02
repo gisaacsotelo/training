da9cf82b079034b4434f3c25395898f2078f8bb0
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-03 01:14:37 +1200

    Test: handlebars w hbs module only

--- server.js
+++ server.js
@@ -4,20 +4,33 @@
 const express = require('express')
 // require file system module - not installed individually since its included in node
 const fs = require('fs')
+// require handlebars
+// const hbs = require('express-handlebars')
+const hbs = require('hbs')
 
 // create our server app in server variable
 const server = express()
 // ^ -------------------------------------------
-// ^ ------- MIDDLEWARE -- server configuration
+// ^ ------- MIDDLEWARE
+
+// ? -- server configuration
 // setting server(app) to use public folder for accesing static files
 server.use(express.static('public'))
+// server.use(express.urlencoded({ extended: false }))
+
+// ? -- Handlebars config
+// setting handlebars to use .hbs as extension
+// server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+// allowing our server(app) to use handlebars - register view engine
+server.set('view engine', 'hbs')
 
 // ^ -------------------------------------------
 // ^ ------- ROUTES
 
 // & -- home route
 server.get('/', (req, res) => {
-  res.send(`We got a home route cuzzy!`)
+  // res.send(`We got a home route cuzzy!`)
+  res.render('home')
 })
 
 // export our server variable: to index.js for testing reasons in the future
