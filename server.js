021fc3ddd4845202e5bdccc4a244fe0590ecdc53
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-02 22:39:10 +1200

    test home route

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+// ^ ------------------------------------------
+// ^ ----------- REQUIRE MODULES --------------
 // require our express module
 const express = require('express')
+// require file system module - not installed individually since its included in node
+const fs = require('fs')
 
 // create our server app in server variable
 const server = express()
+// ^ -------------------------------------------
+// ^ ------- MIDDLEWARE -- server configuration
+// setting server(app) to use public folder for accesing static files
+server.use(express.static('public'))
+
+// ^ -------------------------------------------
+// ^ ------- ROUTES
+
+// & -- home route
+server.get('/', (req, res) => {
+  res.send(`We got a home route cuzzy!`)
+})
 
 // export our server variable: to index.js for testing reasons in the future
 module.exports = server
