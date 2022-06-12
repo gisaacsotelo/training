69cf1fc922943996dcd4a65af2d1f7a60a3ae91c
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-06-12 17:58:02 +1200

    add todo list as js file

---
+++ todo.js
@@ -0,0 +1,11 @@
+/* 
+Just a file used as todo list since I like the comments in js better.
+
+Todo:---------------------
+
+- Create a new database (table) w all the views provided
+  create a loop to print all the options w handlebars.
+
+Todo:---------------------
+
+*/
